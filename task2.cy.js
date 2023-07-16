e34506f93e068ba9950de2ecf0a42d9d641e7907
NC HASSAN ABRAR <89591062+hassnabrar@users.noreply.github.com>
2023-07-16 16:11:39 +0500

    Update task2.cy.js
    Reduced the test cases to 2 and modified the code. The 2nd test case just verifies that resizable box doesn't go below 150 px and should give error.

--- task2.cy.js
+++ task2.cy.js
@@ -1,47 +1,42 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
 describe('Interaction Page Tests', () => {
-    beforeEach(() => {
-      cy.visit('https://demoqa.com/')
-    })
-  
-    it('should display "TOOLS QA" page', () => {
-      cy.contains('TOOLS QA').should('be.visible')
-    })
-  
-    it('should navigate to Interaction Page', () => {
-      cy.contains('Interactions').click()
-      cy.contains('Interaction Page').should('be.visible')
-    })
-  
-    it('should verify sidebar tabs', () => {
-      const sidebarTabs = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application']
-      cy.get('.sidebar-wrapper .menu-list').should('contain', sidebarTabs)
-    })
-  
-    it('should navigate to Resizable Tab', () => {
-      cy.contains('Interactions').click()
-      cy.contains('Resizable').click()
-      cy.contains('Resizable').should('be.visible')
-    })
-  
-    it('should verify current height and width of Box 1', () => {
-      cy.get('.react-resizable-box').should('have.css', 'height', '200px')
-      cy.get('.react-resizable-box').should('have.css', 'width', '200px')
-    })
-  
-    it('should resize Box 1', () => {
-      cy.get('.react-resizable-box').trigger('mousedown', 'bottom-right')
-      cy.get('.react-resizable-box')
-        .trigger('mousemove', { clientX: 400, clientY: 400 })
-        .trigger('mouseup')
-  
-      cy.get('.react-resizable-box').should('have.css', 'min-height', '150px')
-      cy.get('.react-resizable-box').should('have.css', 'min-width', '150px')
-      cy.get('.react-resizable-box').should('have.css', 'max-width', '500px')
-      cy.get('.react-resizable-box').should('have.css', 'max-height', '300px')
-    })
-  
-    it('should verify Box 2 is resizable', () => {
-      cy.get('#resizableBoxWithRestriction').should('have.class', 'react-resizable')
-    })
-  })
-  
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/');})
+
+  it('main page verification along with interaction page verification', () => {
+    cy.get('#fixedban').should('be.visible');
+    cy.contains('Interactions').click();
+    cy.contains('Elements').should('contain', 'Elements');
+    cy.contains('Forms').should('contain', 'Forms');
+    cy.contains('Alerts, Frame & Windows').should('contain', 'Alerts, Frame & Windows');
+    cy.contains('Widgets').should('contain', 'Widgets');
+    cy.contains('Interactions').should('contain', 'Interactions');
+    cy.contains('Book Store Application').should('contain', 'Book Store Application');
+    cy.contains('Resizable').click();
+    cy.get('[id="item-2"]').should('be.visible');
+    cy.get('[class="constraint-area"]').should('be.visible');
+    cy.get('#resizableBoxWithRestriction').should('have.css', 'height', '200px');
+    cy.get('#resizableBoxWithRestriction').should('have.css', 'width', '200px');
+    //cy.contains('Resizable').click();
+    cy.get('#resizableBoxWithRestriction').trigger('mousedown', { which: 1 });
+    cy.get('#resizableBoxWithRestriction').trigger('mousemove', { clientX: 400, clientY: 400 });
+    cy.get('#resizableBoxWithRestriction').trigger('mouseup');
+    cy.get('#resizableBoxWithRestriction .react-resizable-handle').trigger('mousedown', { which: 1 });
+    cy.get('#resizableBoxWithRestriction .react-resizable-handle').trigger('mousemove', { clientX: 150, clientY: 150 });
+    cy.get('#resizableBoxWithRestriction .react-resizable-handle').trigger('mouseup');
+    cy.get('[class="constraint-area"]').should('have.css', 'width', '500px');
+    cy.get('[class="constraint-area"]').should('have.css', 'height', '300px');
+    cy.get('[class="resizable-nolimit mt-4"]').click();
+    cy.get('[class="resizable-nolimit mt-4"] .react-resizable-handle').should('be.visible');
+  });
+  it('Checking if resizable box goes below 150 x 150 or not (This test should give error)', () => {
+    cy.contains('Interactions').click();
+    cy.contains('Resizable').click();
+    cy.get('[class="constraint-area"]').should('have.css', 'width', '149px');
+    cy.get('[class="constraint-area"]').should('have.css', 'height', '149px');
+  });
+ });
